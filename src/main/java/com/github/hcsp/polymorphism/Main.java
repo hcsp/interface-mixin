c701fc4ce34257c2be30e663828830bc60dd42c1
Li-saltair <huagongyilby@126.com>
2019-08-12 13:23:22 +0800

    使用接口的默认方法实现mixin (#24)
    使用接口的默认方法实现mixin (#24)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,47 +1,24 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
+    interface 会跑{
+        String getName();
+        default void 跑(){System.out.println(this.getName() + "跑啊跑啊跑");}
+    }
+    interface 会游泳{
+        String getName();
+        default void 游泳(){System.out.println(this.getName() + "游啊游啊游");};
+    }
 
-        会跑(String name) {
+    abstract static class 动物{
+        private String name;
+
+        public 动物(String name) {
             this.name = name;
         }
 
-        String getName() {
+        public String getName() {
             return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
-        }
-    }
-
-    abstract static class 会游泳 {
-        String name;
-
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
-        }
-    }
-
-    static class 猫 extends 会跑 {
-        猫() {
-            super("小花猫");
-        }
-    }
-
-    static class 鱼 extends 会游泳 {
-        鱼() {
-            super("鱼");
         }
     }
 
@@ -49,8 +26,9 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+     static class 乌龟 extends 动物 implements 会跑,会游泳 {
+        乌龟(){super("乌龟");}
+     }
 
     public static void main(String[] args) {
         new 乌龟().跑();
