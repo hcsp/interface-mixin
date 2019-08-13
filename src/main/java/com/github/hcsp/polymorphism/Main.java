384b60649a5d75f0c052ba07d664f3aa83babcbb
Mingru Li <limingru1991@gmail.com>
2019-08-13 17:03:21 +0800

    接口练习 (#25)
    接口练习 (#25)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,59 +1,88 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
 
-        会跑(String name) {
-            this.name = name;
-        }
+	static interface 会跑 {
+		default void 跑() {
+			System.out.println(this.getClass().getSimpleName() + "跑啊跑啊跑");
+		};
+	}
 
-        String getName() {
-            return name;
-        }
+	static interface 会游泳 {
+		default void 游泳() {
+			System.out.println(this.getClass().getSimpleName() + "游啊游啊游");
+		}
+	}
 
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
-        }
-    }
+//	abstract static class 会跑 {
+//		String name;
+//
+//		会跑(String name) {
+//			this.name = name;
+//		}
+//
+//		String getName() {
+//			return name;
+//		}
+//
+//		public void 跑() {
+//			System.out.println(name + "跑啊跑啊跑");
+//		}
+//	}
+//
+//	abstract static class 会游泳 {
+//		String name;
+//
+//		会游泳(String name) {
+//			this.name = name;
+//		}
+//
+//		String getName() {
+//			return name;
+//		}
+//
+//		public void 游泳() {
+//			System.out.println(name + "游啊游啊游");
+//		}
+//	}
 
-    abstract static class 会游泳 {
-        String name;
 
-        会游泳(String name) {
-            this.name = name;
-        }
+	static class 猫 implements 会跑{
+		private String name;
+		猫(String name) {
+			this.name = name;
+		}
 
-        String getName() {
-            return name;
-        }
+		@Override
+		public void 跑() {
+			System.out.println(name + "跑啊跑啊跑");
+		}
+	}
 
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
-        }
-    }
+	static class 鱼 implements 会游泳{
+		private String name;
+		鱼(String name) {
+			this.name = name;
+		}
 
-    static class 猫 extends 会跑 {
-        猫() {
-            super("小花猫");
-        }
-    }
+		@Override
+		public void 游泳() {
+			System.out.println(name + "游啊游啊游");
+		}
+	}
 
-    static class 鱼 extends 会游泳 {
-        鱼() {
-            super("鱼");
-        }
-    }
+	static class 乌龟 implements 会跑, 会游泳 {
+	}
 
-    // 现在 乌龟 想要同时复用会跑和会游泳的代码
-    // 请尝试通过接口的默认方法实现mixin
-    // 从而实现最大程度的代码复用
-    //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+	// 现在 乌龟 想要同时复用会跑和会游泳的代码
+	// 请尝试通过接口的默认方法实现mixin
+	// 从而实现最大程度的代码复用
+	//
+	// static class 乌龟 extends 会跑, 会游泳 {
+	// }
 
-    public static void main(String[] args) {
-        new 乌龟().跑();
-        new 乌龟().游泳();
-    }
+	public static void main(String[] args) {
+		new 乌龟().跑();
+		new 乌龟().游泳();
+	}
 }
