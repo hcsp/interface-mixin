bd429f2b2fce7aa1ae4ac894176a1121f961c6b7
kungua <52815012+kungua@users.noreply.github.com>
2019-08-27 18:06:31 +0800

    finish mixin (#31)
    finish mixin (#31)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,56 +1,45 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
+    interface 会跑 {
+        default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
+        }
 
-        会跑(String name) {
+        String getName();
+    }
+
+    interface 会游泳 {
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
+        }
+
+        String getName();
+    }
+
+    static class Animal {
+        private String name;
+
+        Animal(String name) {
             this.name = name;
         }
 
-        String getName() {
-            return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+        public String getName() {
+            return this.name;
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
-
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
-        }
-    }
-
-    static class 猫 extends 会跑 {
+    static class 猫 extends Animal implements 会跑 {
         猫() {
-            super("小花猫");
+            super("猫");
         }
     }
 
-    static class 鱼 extends 会游泳 {
-        鱼() {
-            super("鱼");
+    static class 乌龟 extends Animal implements 会跑, 会游泳 {
+        乌龟() {
+            super("乌龟");
         }
     }
-
-    // 现在 乌龟 想要同时复用会跑和会游泳的代码
-    // 请尝试通过接口的默认方法实现mixin
-    // 从而实现最大程度的代码复用
-    //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
 
     public static void main(String[] args) {
         new 乌龟().跑();
