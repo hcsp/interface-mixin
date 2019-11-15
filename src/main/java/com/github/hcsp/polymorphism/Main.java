18822b4dbfd0742ece056775996819362a36ee6a
Kelvin Chen <wheelchen97@gmail.com>
2019-11-16 00:12:44 +0800

    策略模式 (#34)
    策略模式 (#34)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,47 +1,48 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
+    static abstract class 动物{
         String name;
-
-        会跑(String name) {
+        动物(String name) {
             this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
+    interface 会跑 {
+        String getName();
 
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+        default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
         }
     }
 
-    static class 猫 extends 会跑 {
+    interface 会游泳 {
+        String getName();
+
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
+        }
+    }
+
+    static class 猫 extends 动物 implements 会跑{
         猫() {
             super("小花猫");
         }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends 动物 implements 会游泳{
         鱼() {
             super("鱼");
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
         }
     }
 
@@ -49,8 +50,17 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+    static class 乌龟 extends 动物 implements 会跑, 会游泳 {
+        乌龟() {
+            super("乌龟");
+        }
+
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+    }
 
     public static void main(String[] args) {
         new 乌龟().跑();
